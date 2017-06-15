ebb154c55180c0eeddcbeedb2c1393e2659293e5
wenweihu86 <wenweihu86@163.com>
2017-06-15 23:03:51 +0800

    add broker api proto

---
+++ distmq-client/src/main/java/com/github/wenweihu86/distmq/client/api/BrokerAPI.java
@@ -0,0 +1,9 @@
+package com.github.wenweihu86.distmq.client.api;
+
+/**
+ * Created by wenweihu86 on 2017/6/15.
+ */
+public interface BrokerAPI {
+    BrokerMessage.SendMessageResponse sendMessage(BrokerMessage.SendMessageRequest request);
+    BrokerMessage.PullMessageResponse pullMessage(BrokerMessage.PullMessageRequest request);
+}
